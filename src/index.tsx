d149b151ca5f79a96d2428c58588f19ac571fddf
TRAN Quoc-Hao <TRANQH@ad.agf.fr>
2019-06-07 00:53:06 +0200

    wire up store and reducers

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from "redux"
 import { Provider } from "react-redux"
-import thunk from "redux-thunk"
-import { composeWithDevTools as composeEnhancer } from 'redux-devtools-extension';
+
+import { store } from "reducers"
 
 import { App } from "components"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/index.ts
@@ -0,0 +1,31 @@
+import { combineReducers } from "redux"
+import { reducer as form } from "redux-form"
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { composeWithDevTools as composeEnhancer } from 'redux-devtools-extension';
+
+import { _ } from "utils"
+import { metadatas_reducer } from "components/Dashboard/Metadatas/metadatas_reducer"
+// import { configs_reducer } from "components/Dashboard/Configs/configs_reducer"
+// import { dashboard_add_modal_reducer } from "components/Dashboard/Add/dashboard_add_modal_reducer"
+// import { parametres_reducer } from "components/Profile/Parametres/parametres_reducer"
+// import { parametres_modal_reducer } from "components/Profile/Menu/ParametresModal/parametres_modal_reducer"
+// import { edit_config_modal_reducer } from "components/Dashboard/Configs/EditConfigModal/edit_config_modal_reducer"
+
+let all: { [key: string]: _.type.func } = {
+  form,
+  metadatas_reducer,
+  // configs_reducer,
+  // dashboard_add_modal_reducer,
+  // parametres_reducer,
+  // parametres_modal_reducer,
+  // edit_config_modal_reducer
+}
+
+let reducers_name: { [key: string]: string } = {}
+for (let key in all) {
+  reducers_name[all[key].name.toUpperCase()] = all[key].name
+}
+
+export const store = createStore(combineReducers(all), composeEnhancer(applyMiddleware(thunk)))
+export { reducers_name }
