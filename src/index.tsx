ccfe3cf4a1e5f2048d123059c09ec5f628113c70
TRAN Quoc-Hao <TRANQH@ad.agf.fr>
2019-06-06 23:03:43 +0200

    fix bugs redux dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, compose } from "redux"
 import { Provider } from "react-redux"
 import thunk from "redux-thunk"
+import { composeWithDevTools as composeEnhancer } from 'redux-devtools-extension';
 
 import { App } from "components"
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
