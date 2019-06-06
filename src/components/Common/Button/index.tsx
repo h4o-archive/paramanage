8fb33fd76939b4b3ae7766ced3e08d96935444ce
TRAN Quoc-Hao <TRANQH@ad.agf.fr>
2019-06-06 21:47:15 +0200

    create Button component in components/Common

---
+++ src/components/Common/Button/index.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+import { _ } from "utils"
+
+import "./icon_spin.css"
+
+interface ButtonProps {
+  icon?: string,
+  label: string,
+  btn?: string,
+  transparent?: boolean,
+  style?: React.CSSProperties,
+  onClick?: _.type.func
+}
+export const Button: React.FunctionComponent<ButtonProps> = ({ icon, label = "Button", btn, transparent, style, onClick }) => {
+
+  const ButtonContent: React.FunctionComponent = () => {
+    switch (type) {
+      case "icon":
+        return <i className={`${type} ${icon}`}></i>
+      default:
+        return <p>{label}</p>
+    }
+  }
+
+  let type = icon ? "icon" : ""
+  let className = `ui button ${type} ${btn || ""} ${transparent ? "basic" : ""}`
+  let style_overload = { ...style, ...transparent ? { boxShadow: "none", ...style } : {} }
+  return (
+    <button onClick={onClick} className={className} style={style_overload}>
+      <ButtonContent />
+    </button >
+  )
+}
