1488c394b591028f25a9167c2a630b4099d00dd5
TRAN Quoc-Hao <TRANQH@ad.agf.fr>
2019-06-20 09:05:06 +0200

    update windows batch file

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
-set PATH=%PATH%;C:\Hao\Softwares\node-v10.15.3-win-x64
+REM set PATH=%PATH%;.\node-v10.15.3-win-x64
+REM set PATH=%PATH%;C:\Hao\Softwares\node-v10.15.3-win-x64
 npm run config-paramanage-window
 exit
--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
-set PATH=%PATH%;C:\Hao\Softwares\node-v10.15.3-win-x64
+REM set PATH=%PATH%;.\node-v10.15.3-win-x64
+REM set PATH=%PATH%;C:\Hao\Softwares\node-v10.15.3-win-x64
 START /B "" npm run paramanage
 START /B "" npm start
